de5a4a3d95b6b7d3cc9dcfa9b19aa801f15f3892
AdhavanSV02 <adhavansv6981@gmail.com>
2026-05-12 23:39:25 +0530

    Added EchoInstrument Class in utils.py file.

--- echosims/utils.py
+++ echosims/utils.py
@@ -1,5 +1,30 @@
-# import libraries
+import yaml
 
 class EchoInstrument:
-    def __init__(self):
-      pass
+    def __init__(self, filename):
+      self.filename = filename
+      self.channels = self.load_data() 
+    
+    def load_data(self):
+       with open(self.filename, 'r') as f:
+          data = yaml.safe_load(f)
+
+       return data['channels']
+    
+    def get_channel(self, frequency):
+       for channel in self.channels:
+          if channel['frequency'] == frequency:
+             return channel
+          
+       raise ValueError(f'No channel found for frequency {frequency} GHz')
+    
+    def get_frequencies(self):
+       return [channel['frequency'] for channel in self.channels]
+    
+    def get_beam_fwhm(self, frequency):
+       channel = self.get_channel(frequency)
+       return channel['beam_fwhm']
+    
+    def get_polarization_sensitivity(self, frequency):
+       channel = self.get_channel(frequency)
+       return channel['polarization_sensitivity']
